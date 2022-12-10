e157672f8f39335e1162537af0ba922ca2d53100
Bruno Silva <47069622+brunsbr@users.noreply.github.com>
2022-12-10 13:53:28 +0000

    Upload C file

---
+++ Sizeof_test_UAb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+     printf("Olá Mundo!");
+     printf("\nsizeof(char): %ld", sizeof(char));
+     printf("\nsizeof(short): %ld", sizeof(short));
+     printf("\nsizeof(int): %ld", sizeof(int));
+     printf("\nsizeof(long): %ld", sizeof(long));
+     printf("\nsizeof(long) long): %ld", sizeof(long long));
+     printf("\nsizeof(float): %ld", sizeof(float));
+     printf("\nsizeof(double): %ld", sizeof(double));
+     printf("\nsizeof(long double): %ld", sizeof(long double));
+     
+    return 0;
+    
+}
+ 
